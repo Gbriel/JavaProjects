b3181f8b22cd2694868b2f5651a5ac17117fb04e
Gabriel Kopito <kopitog@gmail.com>
2014-04-18 15:16:12 -0700

    2nd commit, adding content

---
+++ CharacterRecognizer.java
@@ -0,0 +1,20 @@
+/*
+  handwriten digit (and char?) identifier
+  via NN. Will take an image (raster?)
+  cut the whitespace from top and bottom,
+  and stretch it to 256X256.
+  
+  Will include a main method for testing,
+  and would like to keep the code well-
+  commented and VC'd via Git.
+  
+  Gabriel Kopito
+  4/18/2014
+*/
+
+public class CharacterRecognizer {
+  public static void main(String[] args) {
+  
+  }
+}
+
